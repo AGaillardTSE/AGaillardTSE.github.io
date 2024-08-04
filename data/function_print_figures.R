2a4102783a2fc04fc0f2006d98fb27c40749d5c0
Alexandre Gaillard <alexandre.gaillard@tse-fr.eu>
2024-08-04 19:00:40 -0400

    Update function_print_figures.R

--- data/function_print_figures.R
+++ data/function_print_figures.R
@@ -1,10 +1,1 @@
- 
-install.packages("RCurl",lib="/media/user", repos='http://cran.us.r-project.org') 
-library("RCurl", lib.loc="/media/user") 
 
-ftpUpload('data_LWS_adjusted.csv', 
-          "ftp://univnantes:VW29DTUNBf4DLdx@ftpperso.free.fr/data_LWS/data_wealth_concentration_2.csv",port=21,verbose=TRUE) 
- 
-ftpUpload('data_LWS_adjusted_JPN.csv', 
-          "ftp://univnantes:VW29DTUNBf4DLdx@ftpperso.free.fr/data_LWS/data_wealth_concentration_2_JPN.csv",port=21,verbose=TRUE) 
- 
